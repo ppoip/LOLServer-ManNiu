0f1522fcaf180afb648d805acf1d96847919b3db
千羽結 <ppoip.c@gmail.com>
2017-03-06 17:00:22 +0800

    init

---
+++ LOLServer/NetFrame/ServerStart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrame
+{
+    public class ServerStart
+    {
+        Socket server;
+        int maxClient;
+
+        public ServerStart(int max,int port)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            maxClient = max;
+        }
+    }
+}
---
+++ LOLServer/NetFrame/UserToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrame
+{
+    /// <summary>
+    /// 用户连接信息
+    /// </summary>
+    public class UserToken
+    {
+
+    }
+}

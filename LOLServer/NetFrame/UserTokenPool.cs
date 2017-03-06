70b37e2f1be8c1e3f10afb68ca3142571a78c21f
千羽結 <ppoip.c@gmail.com>
2017-03-06 18:32:07 +0800

    下班提交

--- LOLServer/NetFrame/UserTokenPool.cs
+++ LOLServer/NetFrame/UserTokenPool.cs
@@ -9,5 +9,36 @@ namespace NetFrame
     public  class UserTokenPool
     {
         private Stack<UserToken> pool;
+
+        public UserTokenPool(int max)
+        {
+            pool = new Stack<UserToken>(max);
+        }
+
+        /// <summary>
+        /// 插入一个连接对象--释放连接
+        /// </summary>
+        /// <param name="token"></param>
+        public void Push(UserToken token)
+        {
+            pool.Push(token);
+        }
+
+        /// <summary>
+        /// 取出一个连接对象--创建连接
+        /// </summary>
+        /// <returns></returns>
+        public UserToken Pop()
+        {
+            return pool.Pop();
+        }
+
+        public int Size
+        {
+            get
+            {
+                return pool.Count;
+            }
+        }
     }
 }
